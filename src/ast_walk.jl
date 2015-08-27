d1e520dec111334e2719debf6595bbbe73102e99
Leonard Truong <leonard.truong@intel.com>
2015-08-27 11:27:21 -0700

    More 'macro-level' nodes

--- src/ast_walk.jl
+++ src/ast_walk.jl
@@ -310,7 +310,7 @@ function from_expr(ast::Any, depth, callback, cbdata, top_level_number, is_top_l
 	    for i = 1:length(args)
 		    args[i] = get_one(from_expr(args[i], depth, callback, cbdata, top_level_number, false, read))
 	    end
-    elseif head == :(+=)
+    elseif head in Set([:(+=), :(/=), :(*=), :(-=)])
         args[1] = get_one(from_expr(args[1], depth, callback, cbdata, top_level_number, false, false))
         args[2] = get_one(from_expr(args[2], depth, callback, cbdata, top_level_number, false, read))
     elseif head == :if
