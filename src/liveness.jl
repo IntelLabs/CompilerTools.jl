f432ad0ceb2a6802583f426b6fb7a5db6d0b1c47
Ehsan Totoni <ehsan.totoni@intel.com>
2016-08-24 15:23:07 -0700

    0.5 fix add transpose() to unmodified liveness functions

--- src/liveness.jl
+++ src/liveness.jl
@@ -752,6 +752,7 @@ function __init__()
   push!(wellknown_all_unmodified, Base.resolve(GlobalRef(Base,:box), force = true))
   push!(wellknown_all_unmodified, Base.resolve(GlobalRef(Base,:arraylen), force = true))
   push!(wellknown_all_unmodified, Base.resolve(GlobalRef(Base,:arraysize), force = true))
+  push!(wellknown_all_unmodified, Base.resolve(GlobalRef(Base,:transpose), force = true))
 #  push!(wellknown_all_unmodified, eval(TopNode(:(!))))
   push!(wellknown_all_unmodified, Base.resolve(GlobalRef(Base.LinAlg,:typeof), force = true))
 
